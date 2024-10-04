7f0e33bbc531885579ab8e3d2cbfdb801edd0625
HaLBH <144716364+HaLBH176637@users.noreply.github.com>
2024-10-04 10:10:09 +0700

    CRUD Admin

--- Webclient/Controllers/StatisticController.cs
+++ Webclient/Controllers/StatisticController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Linq;
 using Webclient.Models;
@@ -16,7 +17,140 @@ namespace Webclient.Controllers
 
         public IActionResult Dashboard()
         {
+            decimal totalMonth = _context.Orders
+                .Where(o => o.Status == "Hoàn thành" && o.CreatedAt.HasValue &&
+                    o.CreatedAt.Value.Month == DateTime.Now.Month &&
+                    o.CreatedAt.Value.Year == DateTime.Now.Year)
+                .Sum(o => o.TotalPrice);
+
+            decimal totalLast7Days = _context.Orders
+                .Where(o => o.Status == "Hoàn thành" &&
+                    o.CreatedAt.HasValue &&
+                    o.CreatedAt >= DateTime.Now.AddDays(-7))
+                .Sum(o => o.TotalPrice);
+
+            ViewBag.TotalMonth = totalMonth;
+            ViewBag.TotalLast7Days = totalLast7Days;
+
             return View();
+        }
+
+        public IActionResult Index()
+        {
+            var products = _context.Products.ToList();
+            return View(products);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                product.CreatedAt = DateTime.Now; 
+                _context.Add(product);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(product);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = _context.Products.Find(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Product product)
+        {
+            if (id != product.ProductId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    product.CreatedAt = DateTime.Now; 
+                    _context.Update(product);
+                    _context.SaveChanges();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!_context.Products.Any(e => e.ProductId == id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(product);
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = _context.Products.FirstOrDefault(m => m.ProductId == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var product = _context.Products.Find(id);
+            if (product != null)
+            {
+                _context.Products.Remove(product);
+                _context.SaveChanges();
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = _context.Products.FirstOrDefault(m => m.ProductId == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
         }
     }
 }
